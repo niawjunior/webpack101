9efa6989f98f5bbd3b4603f76aea92d96afe1872
niawjunior <pasupol@dome.cloud>
2018-01-30 23:16:04 +0700

    html webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
+// import html-plugin
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     // source file
     entry: './src/app.js',
     // output file
     output: {
         // path
-        filename: './dist/app.bundle.js'
-    }
+        path: 'dist',
+        // filename
+        filename: 'app.bundle.js'
+    },
+    // using plugins
+    plugins: [new HtmlWebpackPlugin({
+        // custom title
+        title: 'Project',
+        minify: {
+            collapseWhitespace: true
+        },
+        hash: true,
+
+        // custom html template
+        template: './src/index.ejs',
+    })]
 }
